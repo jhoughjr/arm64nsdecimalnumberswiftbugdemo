96eaa20efcfcd31415112bdf84b8078748df99b8
jimmyhoughjr <jimmyhoughjr@me.com>
2015-02-23 19:06:42 -0600

    Added logs showing what code is executing to better illustrate the issue. Also demosntrated that positive powers do not overflow.

--- Arm64NSDecimalNumberSwiftBugDemo/ViewController.swift
+++ Arm64NSDecimalNumberSwiftBugDemo/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var foo = NSDecimalNumber.one().decimalNumberByMultiplyingByPowerOf10(-3);
+        println("Preparing to call 'var foo = NSDecimalNumber.one().decinalNumberByMultiplyingByPowerOf10(3)'")
+        var foo = NSDecimalNumber.one().decimalNumberByMultiplyingByPowerOf10(3)
         println("foo is \(foo)")
-        
+        println("Preparing to call 'var bar = NSDecimalNumber.one().decinalNumberByMultiplyingByPowerOf10(-3)'")
+        var bar = NSDecimalNumber.one().decimalNumberByMultiplyingByPowerOf10(-3)
+        println("bar is \(bar)")
     }
 
     override func didReceiveMemoryWarning() {
